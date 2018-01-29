cc5d89c9644c3daa31f221d142bb73cf643544b2
Mattias <mattias@debian.debbie>
2018-01-29 07:20:47 +0100

    Fix #128 and more

--- Zuki-shell/gnome-shell/parse-sass.sh
+++ Zuki-shell/gnome-shell/parse-sass.sh
@@ -1,3 +1,13 @@
-#!/usr/bin/bash
+#! /bin/bash
 
-bundle exec sass --update --sourcemap=none .
+if [ ! "$(which sassc 2> /dev/null)" ]; then
+   echo sassc needs to be installed to generate the css.
+   exit 1
+fi
+
+SASSC_OPT="-M -t compact"
+
+echo Generating the css...
+
+sassc $SASSC_OPT gnome-shell.scss gnome-shell.css
+sassc $SASSC_OPT gnome-shell-high-contrast.scss gnome-shell-high-contrast.css
